2a1bf17945538d0147e0e1381452e87b4fc4aa67
sgpushkar <pushkarmhatre424@gmail.com>
2026-08-31 14:32:34 +0530

    feat: add interactive map view and safety area management pages for emergency facility visualization

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -167,8 +167,9 @@ export default function MapView() {
           className="h-[560px] sm:h-[640px] w-full rounded-lg"
         >
           <TileLayer
-            attribution='&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+            maxZoom={20}
           />
 
           {/* User location */}
--- frontend/src/pages/SafeAreas.jsx
+++ frontend/src/pages/SafeAreas.jsx
@@ -295,8 +295,9 @@ export default function SafeAreas() {
               className="h-[520px] w-full rounded-lg"
             >
               <TileLayer
-                attribution='&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                maxZoom={20}
               />
               {userLoc && <MapUpdater center={[userLoc.lat, userLoc.lon]} />}
 
